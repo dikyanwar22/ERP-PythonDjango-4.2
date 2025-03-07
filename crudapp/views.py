2c6163a5c7e80a102bba7beb9383dc6a038c090a
dickyanwar22 <dikyanwar22@gmail.com>
2025-03-07 08:15:59 +0700

    crud django2

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
+from django.db import connection
+
 from .models import Mahasiswa
 from .forms import MahasiswaForm
 
 # Read (Tampilkan data)
+# def index(request):
+#     mahasiswa = Mahasiswa.objects.all()
+#     return render(request, 'index.html', {'mahasiswa': mahasiswa})
+
+#atau bisa juga pakai cara ini
 def index(request):
-    mahasiswa = Mahasiswa.objects.all()
-    return render(request, 'index.html', {'mahasiswa': mahasiswa})
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, nama, nim, jurusan FROM crudapp_mahasiswa")  # Sesuaikan dengan tabel Anda
+        mahasiswa = cursor.fetchall()  # Ambil semua hasil query
+
+    # Mengonversi hasil query ke dalam daftar dictionary
+    mahasiswa_list = []
+    for row in mahasiswa:
+        mahasiswa_list.append({
+            'id': row[0],
+            'nama': row[1],
+            'nim': row[2],
+            'jurusan': row[3]
+        })
+
+    return render(request, 'index.html', {'mahasiswa': mahasiswa_list})
 
 # Create (Tambah data)
 def tambah_mahasiswa(request):
